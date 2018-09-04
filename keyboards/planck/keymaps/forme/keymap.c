3e39c7121c87af8b89b205e53674d8dbab47ebfe
Yoshihisa Tanaka <yt.hisa@gmail.com>
2018-09-04 15:39:25 +0900

    Update keymap

--- keyboards/planck/keymaps/forme/keymap.c
+++ keyboards/planck/keymaps/forme/keymap.c
@@ -48,20 +48,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |Hy/Tab|   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ct/Es|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |   \  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  Up  |
+ * |   \  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  -   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   `  |   \  | Alt  | GUI  |Lower |Space |   -  |Raise |   '  | Left |Right | Down |
+ * |   `  |   \  | Alt  | GUI  |Lower |Space | Sft/-|Raise |   '  | Left |Right |  '   |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,               KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,          KC_Y,           KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,  KC_G,          KC_H,           KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT },
-  {KC_BSLS,              KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,          KC_N,           KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_UP  },
-  {KC_GRV,               KC_BSLS, KC_LALT, KC_LGUI, LOWER, SFT_T(KC_SPC), SFT_T(KC_MINS), RAISE, KC_QUOT, KC_LEFT, KC_RGHT, KC_DOWN}
+  {MT(MOD_HYPR, KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT },
+  {KC_BSLS,              KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_MINS},
+  {KC_GRV,               KC_BSLS, KC_LALT, KC_LGUI, LOWER, KC_SPC, SFT_T(KC_MINS), RAISE, KC_QUOT, KC_LEFT, KC_RGHT, KC_QUOT}
 },
 
 /* Colemak
@@ -122,18 +122,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |      | Home |  Up  |  End |Pg Up |      |  F1  |  F2  |  F3  |  F4  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl | CADwn| Left | Down |Right |Pg Dn |      |  F5  |  F6  |  F7  |  F8  |  :=  |
+ * | Ctrl | CADwn| Left | Down |Right |Pg Dn |  :=  |  F5  |  F6  |  F7  |  F8  |  '   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |DMPLY1|DMPLY2|      |      |      |      |  F9  |  F10 |  F11 |  F12 |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |Space |   -  |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |Space |   -  |      |   `  |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_GRV,  _______,             KC_HOME,         KC_UP,   KC_END,  KC_PGUP, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______},
-  {KC_LCTL, LGUI(LALT(KC_DOWN)), KC_LEFT,         KC_DOWN, KC_RGHT, KC_PGDN, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   MACRO_COLON_EQUAL},
-  {_______, DYN_MACRO_PLAY1,     DYN_MACRO_PLAY2, _______, _______, _______, _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______},
-  {_______, _______,             KC_LALT,         KC_LGUI, _______, KC_SPC,  KC_MINS, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
+  {KC_GRV,  _______,             KC_HOME,         KC_UP,   KC_END,  KC_PGUP, _______,           KC_F1,   KC_F2,  KC_F3,   KC_F4,   _______},
+  {KC_LCTL, LGUI(LALT(KC_DOWN)), KC_LEFT,         KC_DOWN, KC_RGHT, KC_PGDN, MACRO_COLON_EQUAL, KC_F5,   KC_F6,  KC_F7,   KC_F8,   KC_QUOT},
+  {_______, DYN_MACRO_PLAY1,     DYN_MACRO_PLAY2, _______, _______, _______, _______,           KC_F9,   KC_F10, KC_F11,  KC_F12,  _______},
+  {_______, _______,             KC_LALT,         KC_LGUI, _______, KC_SPC,  KC_MINS,           _______, KC_GRV, _______, _______, _______}
 },
 
 /* Raise
@@ -193,7 +193,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
+ * | Reset|      |      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|Plover|      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -203,7 +203,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
-  {_______,      RESET,          DEBUG,          _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL },
+  {RESET,        _______,        DEBUG,          _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL },
   {_______,      _______,        MU_MOD,         AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  PLOVER,  _______},
   {DYN_REC_STOP, DYN_REC_START1, DYN_REC_START2, MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______},
   {_______,      _______,        _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______}
