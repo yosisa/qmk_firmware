64d4d78b6d4f7530efdbe091c7a1e96ced449a9e
Yoshihisa Tanaka <yt.hisa@gmail.com>
2018-10-01 15:17:05 +0900

    Update keymap

--- keyboards/planck/keymaps/forme/keymap.c
+++ keyboards/planck/keymaps/forme/keymap.c
@@ -60,10 +60,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {MT(MOD_HYPR, KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT },
-  {KC_BSLS,              KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_UP  },
-  {KC_GRV,               KC_BSLS, KC_LALT, KC_LGUI, LOWER, KC_SPC, SFT_T(KC_MINS), RAISE, KC_QUOT, KC_LEFT, KC_RGHT, KC_DOWN}
+  {MT(MOD_HYPR, KC_TAB), KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,          KC_Y,          KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,  KC_G,          KC_H,          KC_J,  KC_K,    KC_L,    KC_SCLN, KC_MINS},
+  {KC_BSLS,              KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,          KC_N,          KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT},
+  {KC_GRV,               RESET, KC_LALT, KC_LGUI,   LOWER, CTL_T(KC_SPC), SFT_T(KC_ENT), RAISE, KC_LSFT, KC_LEFT, KC_RGHT, KC_DOWN}
 },
 
 [_OSAKA] = {
