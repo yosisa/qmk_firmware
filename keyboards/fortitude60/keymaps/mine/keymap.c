d1e24e627fc275e367ea9a07569abdc4974701ab
Yoshihisa Tanaka <yt.hisa@gmail.com>
2020-09-05 18:32:18 +0900

    Add ctrl layout for better ctrl-space

--- keyboards/fortitude60/keymaps/mine/keymap.c
+++ keyboards/fortitude60/keymaps/mine/keymap.c
@@ -12,6 +12,7 @@ extern keymap_config_t keymap_config;
 #define _DVORAK 2
 #define _LOWER 3
 #define _RAISE 4
+#define _CTRL 5
 #define _ADJUST 16
 
 enum custom_keycodes {
@@ -20,15 +21,17 @@ enum custom_keycodes {
   DVORAK,
   LOWER,
   RAISE,
+  RAISE2,
   ADJUST,
 };
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
 #define MAGIC CTL_T(KC_LANG2)
+#define CTRL LM(_CTRL, MOD_LCTL)
+#define ALT OSM(MOD_LALT)
+#define GUI OSM(MOD_LGUI)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -46,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `---------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,                   KC_6,            KC_7,  KC_8,    KC_9,    KC_0,    KC_EQL, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,                   KC_Y,            KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_ESC,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,                   KC_H,            KC_J,  KC_K,    KC_L,    KC_SCLN, KC_MINS, \
-  KC_BSLS, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B, KC_ENT, KC_SPC, KC_N,            KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT , \
-                 KC_LEFT, KC_LGUI, LOWER, KC_LCTL,  KC_SPC,  KC_ENT, SFT_T(KC_LANG1), RAISE, KC_LALT, KC_RGHT \
+  KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,                 KC_6,    KC_7,  KC_8,    KC_9,    KC_0,    KC_EQL, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,                 KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_ESC,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,                 KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_MINS, \
+  KC_BSLS, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,    ALT,    GUI, KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT , \
+                 KC_LEFT,     GUI, LOWER,  CTRL, KC_SPC, KC_ENT, KC_LSFT, RAISE, ALT,     KC_RGHT \
 ),
 
 /* Colemak
@@ -135,6 +138,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                    _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, \
                     _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______\
+),
+
+[_CTRL] = LAYOUT( \
+  _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, \
+                    _______, _______, _______, _______, _______, _______,  _______,  RAISE2, _______, _______\
 ),
 
 /* Adjust (Lower + Raise)
@@ -239,6 +250,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             register_code(KC_LANG1);
             unregister_code(KC_LANG1);
           }
+        }
+      }
+      next = false;
+      break;
+    case RAISE2:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+
+        if (IS_LAYER_OFF(_LOWER) && KEYEQ(record->event.key, last_record.event.key) && TIMER_DIFF_16(record->event.time, last_record.event.time) < TAPPING_TERM) {
+          register_code(KC_SPC);
+          unregister_code(KC_SPC);
         }
       }
       next = false;
